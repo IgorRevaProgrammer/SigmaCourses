9a4ab79380806e435b97fd85e12f04fd4e847345
игорь рева <igoryan.reva.01@mail.ru>
2022-02-09 17:10:53 +0200

    Demo 2

---
+++ HomeTask-3/homework3/AlphaNumericCollector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    internal class AlphaNumericCollector
+    {
+        private MyOwnList<string> stringsWithNums;
+        public AlphaNumericCollector()
+        {
+            stringsWithNums=new MyOwnList<string>();
+        }
+        public void AddStringWithNums(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    stringsWithNums.AddToTail(str);
+                    break;
+                }
+            }
+        }
+        public MyOwnList<string> GetStringsWithNums() => stringsWithNums;
+    }
+}
